9de400b246beb6571596aa5bdd8f4242a8795e71
Patrick Nikoletich <patrick@isolated.dev>
2022-12-05 20:08:37 -0800

    Line count tool

---
+++ lineCount.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+#filters = ['migration', 'test']
+filters = []
+
+def file_lines(fname):
+    try:
+        i=0
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
+    except:
+        return 0
+
+# find all files in a directory with extension recursively
+def count_files(dir, filters, print_file_count, print_directory_count, first_caller):
+    count = 0
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            file_path = os.path.join(root, name)
+            if name.endswith('.py'):
+                file_count = file_lines(file_path)
+                count += file_count
+                if print_file_count:
+                    print("{} : {}".format(file_path,file_count))
+        for name in dirs:
+            folder_path = os.path.join(root, name)
+            directory_count = count_files(folder_path, filters, print_file_count, print_directory_count, False)
+            if first_caller and print_directory_count and folder_path.count("/") < 2:
+                print("{} : {}".format(os.path.join(root, name), directory_count))
+        
+    return count
+
+directory = len(sys.argv) > 1 and sys.argv[1] or "./"
+print_recursive =  len(sys.argv) > 2 and any(argument == "-r" for argument in sys.argv[2:])
+print_file_count =  len(sys.argv) > 2 and any(argument == "-f" for argument in sys.argv[2:])
+print_directory_count =  len(sys.argv) > 2 and any(argument == "-d" for argument in sys.argv[2:])
+
+print("Scanning {} {}...".format("recursively" if print_recursive else "top of", directory))
+print("Flags: {} {} {}".format("print_file_count" if print_file_count else "", "print_directory_count" if print_directory_count else "", "print_recursive" if print_recursive else ""))
+print(count_files(directory, filters, print_file_count, True, True))
+
